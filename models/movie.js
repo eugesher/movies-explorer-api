dd324fdfe39191b9d4db9d777a3e9fc5acadd5f9
Evgenii Chernyshov <mailbox@eugesher.site>
2021-05-03 15:56:50 +0300

    feat: movie schema

---
+++ models/movie.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const movieSchema = new mongoose.Schema({
+  country: {
+    type: String,
+    required: [true, "Поле 'страна' не может быть пустым"],
+  },
+  director: {
+    type: String,
+    required: [true, "Поле 'режиссёр' не может быть пустым"],
+  },
+  duration: {
+    type: Number,
+    required: [true, "Поле 'длительность' не может быть пустым"],
+  },
+  year: {
+    type: String,
+    required: [true, "Поле 'год выпуска' не может быть пустым"],
+  },
+  description: {
+    type: String,
+    required: [true, "Поле 'описание' не может быть пустым"],
+  },
+  image: {
+    type: String,
+    required: [true, "Поле 'ссылка на постер' не может быть пустым"],
+    validate: [validator.isURL, "Поле 'ссылка на постер' содержит недопустимое значение"],
+  },
+  trailer: {
+    type: String,
+    required: [true, "Поле 'ссылка на трейлер' не может быть пустым"],
+    validate: [validator.isURL, "Поле 'ссылка на трейлер' содержит недопустимое значение"],
+  },
+  thumbnail: {
+    type: String,
+    required: [true, "Поле 'ссылка на превью' не может быть пустым"],
+    validate: [validator.isURL, "Поле 'ссылка на превью' содержит недопустимое значение"],
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+  },
+  movieId: {
+    required: true,
+  },
+  nameRU: {
+    type: String,
+    required: true,
+  },
+  nameEN: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('movie', movieSchema);
