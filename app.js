c93fc847374fa8db999a4370a52c120673a78842
Evgenii Chernyshov <mailbox@eugesher.site>
2021-05-04 20:25:04 +0300

    feat: error handler

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const { errors } = require('celebrate');
 
 const { login, createUser } = require('./controllers/users');
 const auth = require('./middlewares/auth');
 const router = require('./routes');
+const NotFoundError = require('./errors/not-found-error');
+const errorHandler = require('./errors/error-handler');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -26,6 +29,13 @@ app.post('/signin', login);
 app.post('/signup', createUser);
 app.use(auth);
 app.use(router);
+
+// noinspection JSCheckFunctionSignatures
+app.use(errors());
+app.use((req, res, next) => {
+  next(new NotFoundError('Запрашиваемый ресурс не найден'));
+});
+app.use(errorHandler);
 
 app.listen(PORT);
 
