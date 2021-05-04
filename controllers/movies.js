5f03f451b054a021801dce5f1434fe5d8975e172
Evgenii Chernyshov <mailbox@eugesher.site>
2021-05-04 19:53:32 +0300

    feat: movies controller

---
+++ controllers/movies.js
@@ -0,0 +1,60 @@
+const Movie = require('../models/movie');
+
+const BadRequestError = require('../errors/bad-request-error');
+const NotFoundError = require('../errors/not-found-error');
+const ForbiddenError = require('../errors/forbidden-error');
+const { concatenateErrors } = require('../utils');
+
+module.exports.getMovies = (req, res, next) => {
+  Movie.find({})
+    .then((movies) => res.send(movies))
+    .catch(next);
+};
+
+module.exports.createMovie = (req, res, next) => {
+  Movie.create({
+    country: req.body.country,
+    director: req.body.director,
+    duration: req.body.duration,
+    year: req.body.year,
+    description: req.body.description,
+    image: req.body.image,
+    trailer: req.body.trailer,
+    thumbnail: req.body.thumbnail,
+    movieId: req.body.movieId,
+    nameRU: req.body.nameRU,
+    nameEN: req.body.nameEN,
+    owner: req.user._id,
+  })
+    .then((movie) => res.send(movie))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        next(new BadRequestError(concatenateErrors(err)));
+      } else {
+        next(err);
+      }
+    });
+};
+
+module.exports.deleteMovie = (req, res, next) => {
+  const { id } = req.params;
+  Movie.findById(id)
+    .then((movie) => {
+      if (!movie) {
+        throw new NotFoundError('Фильма не существует');
+      } else if (!movie.owner.equals(req.user._id)) {
+        throw new ForbiddenError('Можно удалять только свои фильмы');
+      } else {
+        Movie.findByIdAndRemove(id).then((removedMovie) => {
+          res.send(removedMovie);
+        });
+      }
+    })
+    .catch((err) => {
+      if (err.kind === 'ObjectId') {
+        next(new BadRequestError('Недопустимый идентификатор фильма'));
+      } else {
+        next(err);
+      }
+    });
+};
