7e8e5a93cfd6a668cb59ea770f9c647e0e8c81d3
Evgenii Chernyshov <mailbox@eugesher.site>
2021-05-04 20:23:09 +0300

    feat: user authentication

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+const { login, createUser } = require('./controllers/users');
+const auth = require('./middlewares/auth');
+const router = require('./routes');
+
 const { PORT = 3000 } = process.env;
 const app = express();
 
@@ -17,6 +21,11 @@ mongoose.connect('mongodb://localhost:27017/bitfilmsdb', {
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/signin', login);
+app.post('/signup', createUser);
+app.use(auth);
+app.use(router);
 
 app.listen(PORT);
 
