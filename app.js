93ccf45ab0ef33098a1443997db6b0aec0f735a7
Evgenii Chernyshov <mailbox@eugesher.site>
2021-05-05 14:07:17 +0300

    fix: enable logger

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const auth = require('./middlewares/auth');
 const router = require('./routes');
 const NotFoundError = require('./errors/not-found-error');
 const errorHandler = require('./errors/error-handler');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -25,11 +26,16 @@ mongoose.connect('mongodb://localhost:27017/bitfilmsdb', {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// noinspection JSCheckFunctionSignatures
+app.use(requestLogger);
+
 app.post('/signin', login);
 app.post('/signup', createUser);
 app.use(auth);
 app.use(router);
 
+// noinspection JSCheckFunctionSignatures
+app.use(errorLogger);
 // noinspection JSCheckFunctionSignatures
 app.use(errors());
 app.use((req, res, next) => {
