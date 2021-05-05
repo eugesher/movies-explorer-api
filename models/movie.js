f67dd31a7421b9c42bab40d466011ca61187c013
Evgenii Chernyshov <mailbox@eugesher.site>
2021-05-05 14:59:16 +0300

    refactor: movie schema error messages

--- models/movie.js
+++ models/movie.js
@@ -1,57 +1,59 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 
+const { errorMessages, fieldNames } = require('../utils');
+
 const movieSchema = new mongoose.Schema({
   country: {
     type: String,
-    required: true,
+    required: [true, errorMessages.empty(fieldNames.country)],
   },
   director: {
     type: String,
-    required: true,
+    required: [true, errorMessages.empty(fieldNames.director)],
   },
   duration: {
     type: Number,
-    required: true,
+    required: [true, errorMessages.empty(fieldNames.duration)],
   },
   year: {
     type: String,
-    required: true,
+    required: [true, errorMessages.empty(fieldNames.year)],
   },
   description: {
     type: String,
-    required: true,
+    required: [true, errorMessages.empty(fieldNames.description)],
   },
   image: {
     type: String,
-    required: true,
-    validate: validator.isURL,
+    required: [true, errorMessages.empty(fieldNames.image)],
+    validate: [validator.isURL, errorMessages.invalid(fieldNames.image)],
   },
   trailer: {
     type: String,
-    required: true,
-    validate: validator.isURL,
+    required: [true, errorMessages.empty(fieldNames.trailer)],
+    validate: [validator.isURL, errorMessages.invalid(fieldNames.trailer)],
   },
   thumbnail: {
     type: String,
-    required: true,
-    validate: validator.isURL,
+    required: [true, errorMessages.empty(fieldNames.thumbnail)],
+    validate: [validator.isURL, errorMessages.invalid(fieldNames.thumbnail)],
+  },
+  movieId: {
+    type: Number,
+    required: [true, errorMessages.empty(fieldNames.movieId)],
+  },
+  nameRU: {
+    type: String,
+    required: [true, errorMessages.empty(fieldNames.nameRU)],
+  },
+  nameEN: {
+    type: String,
+    required: [true, errorMessages.empty(fieldNames.nameEN)],
   },
   owner: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'user',
-    required: true,
-  },
-  movieId: {
-    type: Number,
-    required: true,
-  },
-  nameRU: {
-    type: String,
-    required: true,
-  },
-  nameEN: {
-    type: String,
     required: true,
   },
 });
--- utils/index.js
+++ utils/index.js
@@ -1,14 +1,25 @@
 module.exports.concatenateErrors = (err) => Object.values(err.errors).map((e) => e.message).join('. ');
 
 module.exports.errorMessages = {
-  invalid: (fieldName) => `Поле ${fieldName} содержит недопустимое значение`,
-  empty: (fieldName) => `Поле ${fieldName} не может быть пустым`,
-  tooShort: (fieldName, length) => `Поле ${fieldName} должно содержать не менее ${length} символов`,
-  tooLong: (fieldName, length) => `Поле ${fieldName} должно содержать не более ${length} символов`,
+  invalid: (fieldName) => `Поле '${fieldName}' содержит недопустимое значение`,
+  empty: (fieldName) => `Поле '${fieldName}' не может быть пустым`,
+  tooShort: (fieldName, length) => `Поле '${fieldName}' должно содержать не менее ${length} символов`,
+  tooLong: (fieldName, length) => `Поле '${fieldName}' должно содержать не более ${length} символов`,
   wrongUserCredentials: 'Неправильные почта или пароль',
 };
 
 module.exports.fieldNames = {
   email: 'email',
   name: 'имя',
+  country: 'страна создания',
+  director: 'режиссёр',
+  duration: 'длительность',
+  year: 'год выпуска',
+  description: 'описание',
+  image: 'ссылка на постер',
+  trailer: 'ссылка на трейлер',
+  thumbnail: 'ссылка на превью',
+  movieId: 'идентификатор фильма',
+  nameRU: 'имя(ru)',
+  nameEN: 'имя(en)',
 };
